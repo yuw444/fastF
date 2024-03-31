0e181a56de50a5555a507be1e15167ce0bdff27d
yuw444 <yuw926@icloud.com>
2024-03-31 00:37:44 -0500

    delete

--- test/test.sql
+++
@@ -1,9 +0,0 @@
-
-SELECT cell_index, feature_index, COUNT(DISTINCT encoded_umi) AS umi_count 
-FROM umi
-GROUP BY cell_index, feature_index;
-
-CREATE TABLE UMIds AS
-SELECT * 
-FROM UMI
-LIMIT 10000;
